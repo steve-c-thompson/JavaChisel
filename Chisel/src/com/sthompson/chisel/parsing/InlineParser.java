7a12a6647b670c5d649dfd668a6ba9d04021106e
Steve Thompson <scthomps312@gmail.com>
2018-07-20 22:06:04 -0600

    Inline Parser progress, but workspace has broken

---
+++ Chisel/src/com/sthompson/chisel/parsing/InlineParser.java
@@ -0,0 +1,13 @@
+package com.sthompson.chisel.parsing;
+
+import java.util.List;
+
+public class InlineParser implements ChiselParser {
+
+	@Override
+	public List<String> parseLines(List<String> input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
